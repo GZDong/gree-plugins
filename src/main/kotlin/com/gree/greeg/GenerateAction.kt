328d1103db6700f241dbb0d8efe598b7896f9c26
GZDong <dong_scau@163.com>
2023-06-26 15:59:58 +0800

    规范插件信息

--- src/main/kotlin/com/gree/greeg/GenerateAction.kt
+++ src/main/kotlin/com/gree/greeg/GenerateAction.kt
@@ -14,23 +14,52 @@ class GenerateAction : AnAction() {
         val project = event.project
         val path = LangDataKeys.VIRTUAL_FILE.getData(event.dataContext)?.path
 
-        var modelName = Messages.showInputDialog(
-            project,
-            "输入类名(比如MainActivity，或者Main)",
-            "自动生成mvvm下的各个组件",
-            Messages.getInformationIcon()
+        val chooseResult = Messages.showYesNoDialog(
+            "选择项目，不同项目的生成内容不同",
+            "选择项目",
+            "配件项目",
+            "mvp项目",
+            Messages.getQuestionIcon()
         )
-        if (modelName != null) {
-            if (!modelName.endsWith("Activity")) {
-                modelName = modelName.plus("Activity")
+        if (chooseResult == 0) {
+            var modelName = Messages.showInputDialog(
+                project,
+                "输入类名(比如MainActivity，或者Main)",
+                "自动生成mvvm下的各个组件",
+                Messages.getInformationIcon()
+            )
+            if (modelName != null) {
+                if (!modelName.endsWith("Activity")) {
+                    modelName = modelName.plus("Activity")
+                }
+                val result = Messages.showYesNoDialog(
+                    "是否需要自动生成repository？",
+                    "下一步",
+                    "需要",
+                    "不需要，下一步",
+                    Messages.getQuestionIcon()
+                )
+                if (result == 0) {
+                    genModelActivity(this, path, modelName, "test/$modelName")
+                    LocalFileSystem.getInstance().refresh(false)
+                } else {
+                    genModelActivity(this, path, modelName, "test/$modelName")
+                    LocalFileSystem.getInstance().refresh(false)
+                }
             }
-            val result = Messages.showYesNoDialog("是否需要自动生成repository？", "下一步", Messages.getQuestionIcon())
-            if (result == 0) {
-                genModelActivity(this, path, modelName, "test/$modelName")
-                LocalFileSystem.getInstance().refresh(false)
-            } else {
-                genModelActivity(this, path, modelName, "test/$modelName")
-                LocalFileSystem.getInstance().refresh(false)
+        } else if (chooseResult == 1) {
+            var modelName = Messages.showInputDialog(
+                project,
+                "输入类名(比如MainActivity，或者Main)",
+                "自动生成mvvm下的各个组件",
+                Messages.getInformationIcon()
+            )
+            if (modelName != null) {
+                if (!modelName.endsWith("Activity")) {
+                    modelName = modelName.plus("Activity")
+                }
+//                genModelActivity(this, path, modelName, "test/$modelName")
+//                LocalFileSystem.getInstance().refresh(false)
             }
         }
     }
