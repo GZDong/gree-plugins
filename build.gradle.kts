54a47313a3155155ffc91d9f97d213a692e5b65d
高志栋 <A80788>
2025-08-14 09:42:00 +0800

    升级相关版本，支持最新AS的运行

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,39 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+    id("org.jetbrains.intellij") version "1.17.3"
 }
 
 group = "com.gree"
-version = "1.1.2"
+version = "1.1.3"
 
 repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
     mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
-    type.set("IC") // Target IDE Platform
+    version.set("2023.2")
+    type.set("IC")
 
-    plugins.set(listOf("Kotlin", "android", "java"))
+    plugins.set(listOf( "android", "java"))
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
-        sinceBuild.set("213")
-        untilBuild.set("223.*")
+        sinceBuild.set("251")
+        untilBuild.set("")
     }
 
     signPlugin {
